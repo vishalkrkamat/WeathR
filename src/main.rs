0a1563123b81c4fb8088b6ab1e0b2dc0156497ac
vishalkrkamat <vishalkrkamat7@gmail.com>
2025-01-03 12:49:19 +0530

    dependcies added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+use serde_json::Value;
+use std::io;
+struct Weather {
+    temperature: String,
+    weatehr: String,
+    aqi: u32,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // The URL of the API endpoint
+    let url = "http://api.weatherapi.com/v1/current.json?key=3295f8e0c45248f69e963550243112&q=shillong&aqi=no";
+
+    // Send an HTTP GET request
+    let response = reqwest::get(url).await?;
+
+    // Parse the response body as JSON
+    let json: serde_json::Value = response.json().await?;
+
+    // Print the JSON data
+    println!("Response: {:#?}", json);
+
+    Ok(())
 }
